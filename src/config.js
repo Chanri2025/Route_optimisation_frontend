9ee8dcd076be2c23524e6d152314a6626979d138
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2025-10-06 15:05:08 +0530

    Frontend New Changes on Progress - XXII

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const API_URL = "http://192.168.1.119:5000/"
-export const API_URL = "http://103.210.73.130:5000/"
+export const API_URL = "http://103.210.73.130:5000"
 // export const API_URL = "http://103.241.147.6:5000/"
 // export const API_URL = "http://172.16.0.167:5000/"
